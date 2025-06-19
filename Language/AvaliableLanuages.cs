98274f4210c405bed71e6b205eac364b68e8d740
kazaminosuke <99616465+kazaminosuke@users.noreply.github.com>
2025-06-19 18:50:10 +0900

    Update AvaliableLanuages.cs

--- Language/AvaliableLanuages.cs
+++ Language/AvaliableLanuages.cs
@@ -26,6 +26,7 @@ namespace BedrockLauncher.Localization.Language
                 "uk-UA",
                 "zh-CN",
                 "zh-TW"
+                "ja-JP"
             };
         }
     }
